8403d09169bff93bb3fd6b0b5e81b4c500a8fca6
Chris Farris <chris.farris@turner.com>
2018-09-26 21:42:51 -0400

    replaced ENV with STACKNAME as first param to the trigger script

--- bin/trigger_state_machine.sh
+++ bin/trigger_state_machine.sh
@@ -2,19 +2,19 @@
 
 RESOURCEID="InventoryProcessStateMachine"
 
-ENV=$1
+STACKNAME=$1
 EVENT=$2
-if [ -z $ENV ] ; then
-    echo "Must specify ENV"
+if [ -z $STACKNAME ] ; then
+    echo "Must specify STACKNAME"
     exit 1
 fi
 
 # If the user didn't pass in an event file, then get the payer list via the stack's parameters (using this ugly jq command)
 if [ -z $EVENT ] ; then
-    EVENT="${ENV}-test-event.json" # file to save the event as
-    PAYERLIST=`aws cloudformation describe-stacks --stack-name antiope-${ENV} | jq -r '.Stacks[].Parameters[]|select(.ParameterKey=="pPayerAccountList").ParameterValue'`
+    EVENT="${STACKNAME}-test-event.json" # file to save the event as
+    PAYERLIST=`aws cloudformation describe-stacks --stack-name ${STACKNAME} | jq -r '.Stacks[].Parameters[]|select(.ParameterKey=="pPayerAccountList").ParameterValue'`
     if [ -z "$PAYERLIST" ] ; then
-        echo "Didn't find the payerlist in stack antiope-${ENV}. Aborting..."
+        echo "Didn't find the payerlist in stack ${STACKNAME}. Aborting..."
         exit 1
     fi
     echo "{\"payer\": [ ${PAYERLIST} ] }" > $EVENT
@@ -24,9 +24,9 @@ elif [ ! -f $EVENT ] ; then
 fi
 
 DATE=`date +%Y-%m-%d-%H-%M`
-STATEMACHINE_ARN=`aws cloudformation describe-stack-resources --stack-name antiope-${ENV} --output text | grep ${RESOURCEID} | awk '{print $3}'`
+STATEMACHINE_ARN=`aws cloudformation describe-stack-resources --stack-name ${STACKNAME} --output text | grep ${RESOURCEID} | awk '{print $3}'`
 if [ -z $STATEMACHINE_ARN ] ; then
-    echo "Unable to find StateMachine Arn for Stack antiope-${ENV}. Aborting.."
+    echo "Unable to find StateMachine Arn for Stack ${STACKNAME}. Aborting.."
     exit 1
 fi
 
