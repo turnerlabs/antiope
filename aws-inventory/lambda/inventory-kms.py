4f4533662e2bbf00d2c661f38e8b7161c556f718
Chris Farris <chris.farris@turner.com>
2018-11-23 16:11:39 -0500

    redid error handling so we can capture AccessDeniedException cases (which happen a lot with KMS)

--- aws-inventory/lambda/inventory-kms.py
+++ aws-inventory/lambda/inventory-kms.py
@@ -17,7 +17,7 @@ logger.setLevel(logging.DEBUG)
 logging.getLogger('botocore').setLevel(logging.WARNING)
 logging.getLogger('boto3').setLevel(logging.WARNING)
 
-KEY_RESOURCE_PATH = "kms/key"
+RESOURCE_PATH = "kms/key"
 
 def lambda_handler(event, context):
     logger.debug("Received event: " + json.dumps(event, sort_keys=True))
@@ -55,29 +55,61 @@ def discover_keys(target_account, region):
 
 def process_key(client, key_arn, target_account, region):
     '''Pull additional information for the key, and save to bucket'''
-    resource_name = "{}-{}-{}".format(target_account.account_id, region, key['KeyId'].replace('/', '-'))
+
 
     # Enhance Key Information to include CMK Policy, Aliases, Tags
     key = client.describe_key(KeyId=key_arn)['KeyMetadata']
-    aliases = get_key_aliases(client, key_arn)
-    if aliases:
-        key['Aliases'] = aliases
 
-    policy = get_key_policy(client, key_arn)
-    if aliases:
-        key['ResourcePolicy'] = policy
+    try:
+        aliases = get_key_aliases(client, key_arn)
+        if aliases:
+            key['Aliases'] = aliases
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'NotFoundException':
+            pass
+        elif e.response['Error']['Code'] == 'AccessDeniedException':
+            key['Aliases-Error'] = e.response['Error']['Message']
+        else:
+            raise
 
-    tags = get_key_tags(client, key_arn)
-    if tags:
-        key['Tags'] = tags
-    
-    grants = get_key_grants(client, key_arn)
-    if grants:
-        key['Grants'] = grants
+    try:
+        policies = get_policy_list(client, key_arn)
+        policy = get_key_policy(client, key_arn, policies)
+        if policy:
+            key['ResourcePolicy'] = policy
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'NotFoundException':
+            pass
+        elif e.response['Error']['Code'] == 'AccessDeniedException':
+            key['ResourcePolicy-Error'] = e.response['Error']['Message']
+        else:
+            raise
 
-    # Remove redundant key
-    key.pop('AWSAccountId')
+    try:
+        tags = get_key_tags(client, key_arn)
+        if tags:
+            key['Tags'] = tags
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'NotFoundException':
+            pass
+        elif e.response['Error']['Code'] == 'AccessDeniedException':
+            key['Tags-Error'] = e.response['Error']['Message']
+        else:
+            raise
 
+    try:
+        grants = get_key_grants(client, key_arn)
+        if grants:
+            key['Grants'] = grants
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'NotFoundException':
+            pass
+        elif e.response['Error']['Code'] == 'AccessDeniedException':
+            key['Grants-Error'] = e.response['Error']['Message']
+        else:
+            raise
+
+    resource_name = "{}-{}-{}".format(target_account.account_id, region, key['KeyId'].replace('/', '-'))
     key['resource_type']     = "kms-key"
     key['region']            = region
     key['account_id']        = target_account.account_id
@@ -98,22 +130,17 @@ def get_key_grants(client, key_arn):
     '''
 
     grants = []
-    try:
-        response = client.list_grants(KeyId=key_arn)
-        while response['Truncated']:
-            grants += response['Grants']
-            response = client.list_grants(KeyId=key_arn, Marker=response['NextMarker'])
+    response = client.list_grants(KeyId=key_arn)
+    while response['Truncated']:
         grants += response['Grants']
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'NotFoundException':
-            pass
-        else:
-            raise
+        response = client.list_grants(KeyId=key_arn, Marker=response['NextMarker'])
+    grants += response['Grants']
+
     return grants
-    
+
 def get_key_aliases(client, key_arn):
     '''Return List of Aliases for Key
-    
+
     Args:
         client: Boto3 Client, connected to account and region
         key_arn (string): ARN of Key
@@ -124,20 +151,15 @@ def get_key_aliases(client, key_arn):
     '''
 
     aliases = []
-    try:
-        response = client.list_aliases(KeyId=key_arn)
-        while response['Truncated']:
-            aliases += response['Aliases']
-            response = client.list_aliases(KeyId=key_arn, Marker=response['NextMarker'])
+    response = client.list_aliases(KeyId=key_arn)
+    while response['Truncated']:
         aliases += response['Aliases']
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'NotFoundException':
-            pass
-        else:
-            raise
+        response = client.list_aliases(KeyId=key_arn, Marker=response['NextMarker'])
+    aliases += response['Aliases']
+
     return map(lambda x: x['AliasName'], aliases)
 
-def get_key_policy(client, key_arn):
+def get_key_policy(client, key_arn, policies):
     '''Return ResourcePolicy of Key
 
     For right now, the only valid policy name is 'default', as per
@@ -149,36 +171,24 @@ def get_key_policy(client, key_arn):
     Args:
         client: Boto3 Client, connected to account and region
         key_arn (string): ARN of Key
-    
-    Returns: 
+
+    Returns:
         dict: Resource Policy of Key
-    
+
     '''
 
-    policies = get_policy_list(client, key_arn)
     if len(policies) == 1 and policies[0] == 'default':
-        try:
-            response = client.get_key_policy(KeyId=key_arn, PolicyName=policies[0])
-            if 'Policy' in response:
-                return json.loads(response['Policy'])
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'NotFoundException':
-                pass
-            else:
-                raise
+        response = client.get_key_policy(KeyId=key_arn, PolicyName=policies[0])
+        if 'Policy' in response:
+            return json.loads(response['Policy'])
     else:
         for p in policies:
-            try:
-                response = client.get_key_policy(KeyId=key_arn, PolicyName=p)
-                if 'Policy' in response:
-                    policy[p] = json.loads(response['Policy'])
-                return policy
-            except ClientError as e:
-                if e.response['Error']['Code'] == 'NotFoundException':
-                    pass
-                else:
-                    raise
-    
+            response = client.get_key_policy(KeyId=key_arn, PolicyName=p)
+            if 'Policy' in response:
+                policy[p] = json.loads(response['Policy'])
+            return policy
+
+
     # Just in case of NotFoundException
     return None
 
@@ -191,7 +201,7 @@ def get_policy_list(client, key_arn):
 
     Returns:
         dict: Resource Policy of Key
-    
+
     '''
 
     policies = []
@@ -201,6 +211,7 @@ def get_policy_list(client, key_arn):
         response = client.list_key_policies(KeyId=key_arn, Marker=response['NextMarker'])
     policies += response['PolicyNames']
     return policies
+
 
 def get_key_tags(client, key_arn):
     '''Return list of tags for key
@@ -215,22 +226,13 @@ def get_key_tags(client, key_arn):
     '''
 
     unparsed_tags = []
-    try:
-        response = client.list_resource_tags(KeyId=key_arn)
-        while response['Truncated']:
-            unparsed_tags += response('Tags')
-            response = client.list_resource_tags(KeyId=key_arn, Marker=resource['NextMarker'])
-        unparsed_tags += response('Tags')
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'NotFoundException':
-            pass
-        else:
-            raise
-    
-    tags = []
-    for tag in unparsed_tags:
-        tags += kms_parse_tags(tag)
-    return tags
+    response = client.list_resource_tags(KeyId=key_arn)
+    while response['Truncated']:
+        unparsed_tags += response['Tags']
+        response = client.list_resource_tags(KeyId=key_arn, Marker=resource['NextMarker'])
+    unparsed_tags += response['Tags']
+
+    return(kms_parse_tags(unparsed_tags))
 
 def kms_parse_tags(tagset):
     '''Format list of tag to something easily consumable in Splunk
