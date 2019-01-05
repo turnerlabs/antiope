de2ae1157f6e2ec25b4e37bb5e32cbdfd280ce70
Chris Farris <chris.farris@turner.com>
2019-01-05 12:31:33 -0500

    Initial functional GCP Project inventory.

---
+++ gcp-inventory/lambda/pull_organization_data.py
@@ -0,0 +1,87 @@
+import boto3
+from botocore.exceptions import ClientError
+
+from googleapiclient import discovery
+from oauth2client.service_account import ServiceAccountCredentials
+
+from gcp_lib.common import *
+
+import json
+import os
+import time
+
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logging.getLogger('botocore').setLevel(logging.WARNING)
+logging.getLogger('boto3').setLevel(logging.WARNING)
+
+
+# Lambda main routine
+def handler(event, context):
+    logger.info("Received event: " + json.dumps(event, sort_keys=True))
+
+    dynamodb = boto3.resource('dynamodb')
+    project_table = dynamodb.Table(os.environ['PROJECT_TABLE'])
+
+    credential_info = get_gcp_creds(os.environ['GCP_SECRET_NAME'])
+    if credential_info is None:
+        raise Exception("Unable to extract GCP Credentials. Aborting...")
+
+    project_list = get_projects(credential_info)
+    if project_list is None:
+        raise Exception("No Projects found. Aborting...")
+
+    # print(project_list)
+    for p in project_list:
+        create_or_update_project(p, project_table)
+
+    event['project_list'] = project_list
+    return(event)
+
+# end handler()
+
+##############################################
+
+
+def get_projects(credential_info):
+    """
+    Given a credential set, write to DDB table, publish to SNS topic, and return
+    a list of projects for a given account.
+    """
+
+    project_list = []
+    try:
+        credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential_info)
+        service = discovery.build('cloudresourcemanager', 'v1', credentials=credentials, cache_discovery=False )
+    except Exception as e:
+        logger.critical(f"Error during Credential and Service creation: {e}")
+        return(None)
+
+    request = service.projects().list()
+    while request is not None:
+        response = request.execute()
+        for project in response['projects']:
+            project_list.append(project)
+
+        request = service.projects().list_next(
+                      previous_request=request,
+                      previous_response=response
+                  )
+
+    return(project_list)
+
+
+def create_or_update_project(project, project_table):
+    logger.info(u"Adding project {} with name {} and number {}".format(project[u'projectId'], project[u'name'], project[u'projectNumber']))
+
+    try:
+        response = project_table.put_item(Item=project)
+    except ClientError as e:
+        raise AccountUpdateError(u"Unable to create {}: {}".format(project[u'name'], e))
+    except KeyError as e:
+        logger.critical(f"Project {project['projectId']} is missing a key: {e}")
+
+
+class AccountUpdateError(Exception):
+    '''raised when an update to DynamoDB Fails'''
---
+++ gcp-inventory/lambda/report-projects.py
@@ -0,0 +1,100 @@
+import boto3
+from botocore.exceptions import ClientError
+
+import json
+import os
+import time
+import datetime
+
+from gcp_lib.project import *
+from gcp_lib.common import *
+
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logging.getLogger('botocore').setLevel(logging.WARNING)
+logging.getLogger('boto3').setLevel(logging.WARNING)
+
+
+# table_format = {
+#     "account_id": "Account ID",
+#     "account_name": "Account Name",
+#     "payer_name": "Parent",
+#     "root_email": "Root Address",
+#     "assume_role_link": "Assume Role Link"
+# }
+
+
+table_format = ["projectId", "name", "projectNumber", "lifecycleState", "createTime" ]
+
+
+# Lambda main routine
+def handler(event, context):
+    logger.info("Received event: " + json.dumps(event, sort_keys=True))
+
+    dynamodb = boto3.resource('dynamodb')
+    account_table = dynamodb.Table(os.environ['PROJECT_TABLE'])
+
+
+    # We will make a HTML Table and a Json file with this data
+    table_data = ""
+    json_data = []
+
+    # Get and then sort the list of projects by name, case insensitive.
+    project_list = get_active_projects()
+    project_list.sort(key=lambda x: x.projectId.lower())
+
+    for project in project_list:
+        logger.info(f"{project.projectId}")
+
+        j = {}
+        table_data += "<tr>"
+        for col_name in table_format:
+            table_data += "<td>{}</td>".format(getattr(project, col_name))
+            j[col_name] = getattr(project, col_name)
+        table_data += "</tr>\n"
+        json_data.append(j)
+
+
+    s3_client = boto3.client('s3')
+
+    try:
+        response = s3_client.get_object(
+            Bucket=os.environ['INVENTORY_BUCKET'],
+            Key='Templates/project_inventory.html'
+        )
+        html_body = str(response['Body'].read().decode("utf-8") )
+    except ClientError as e:
+        logger.error("ClientError getting HTML Template: {}".format(e))
+        raise
+
+    # This assumes only three {} in the template
+    try:
+        file = html_body.format(os.environ['INVENTORY_BUCKET'], len(project_list), table_data, datetime.datetime.now())
+    except Exception as e:
+        logger.error("Error generating HTML Report. Template correct? : {}".format(e))
+        raise
+
+
+    try:
+        response = s3_client.put_object(
+            # ACL='public-read',
+            Body=file,
+            Bucket=os.environ['INVENTORY_BUCKET'],
+            ContentType='text/html',
+            Key='Reports/gcp_project_inventory.html',
+        )
+
+        # Save the JSON to S3
+        response = s3_client.put_object(
+            # ACL='public-read',
+            Body=json.dumps(json_data, sort_keys=True, indent=2),
+            Bucket=os.environ['INVENTORY_BUCKET'],
+            ContentType='application/json',
+            Key='Reports/gcp_project_inventory.json',
+        )
+    except ClientError as e:
+        logger.error("ClientError saving report: {}".format(e))
+        raise
+
+    return(event)
