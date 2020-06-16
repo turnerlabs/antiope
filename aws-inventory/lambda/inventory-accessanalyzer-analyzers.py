50afcc2612e7b544709f954ab2075d13eb16a58b
Chris Farris <chris@room17.com>
2020-06-16 09:49:00 -0400

    Remove logic to fetch findings

--- aws-inventory/lambda/inventory-accessanalyzer-analyzers.py
+++ aws-inventory/lambda/inventory-accessanalyzer-analyzers.py
@@ -19,9 +19,6 @@ logging.getLogger('urllib3').setLevel(logging.WARNING)
 RESOURCE_PATH = "accessanalyzer/analyzer"
 RESOURCE_TYPE = "AWS::AccessAnalyzer::Analyzer"
 
-FINDING_PATH = "accessanalyzer/finding"
-FINDING_TYPE = "AWS::AccessAnalyzer::Finding"
-
 def lambda_handler(event, context):
     logger.debug("Received event: " + json.dumps(event, sort_keys=True))
     message = json.loads(event['Records'][0]['Sns']['Message'])
@@ -29,18 +26,8 @@ def lambda_handler(event, context):
 
     try:
 
-        # This function is to be attached to both a
-        if 'payer_id' in message:
-            payer_account = AWSAccount(message['payer_id'])
-            target_account = payer_account.get_delegated_admin_account_for_service('access-analyzer')
-            account_id = message['payer_id']
-            org_level = True
-        elif 'account_id' in message:
-            account_id = message['account_id']
-            target_account = AWSAccount(message['account_id'])
-            org_level = False
-        else:
-            raise("Message contained neither account_id nor payer_id")
+        account_id = message['account_id']
+        target_account = AWSAccount(message['account_id'])
 
         regions = target_account.get_regions()
         if 'region' in message:
@@ -50,10 +37,7 @@ def lambda_handler(event, context):
         for r in regions:
             client = target_account.get_client('accessanalyzer', region=r)
             analyzer = get_analyzer(target_account, client, r)
-            if analyzer is None:
-                logger.error(f"No Analyzers configured for {target_account.account_name}({target_account.account_id}) in {r}")
-                continue
-            get_findings(target_account, client, r, analyzer)
+
 
     except AntiopeAssumeRoleError as e:
         logger.error("Unable to assume role into account {}({})".format(target_account.account_name, target_account.account_id))
@@ -105,45 +89,3 @@ def get_analyzer(target_account, client, region):
     # There can currently only be one analyzer. When that changes this part needs to be fixed
     return(analyzers[0]['arn'])  # Arn is needed for getting findings
 
-
-def get_findings(target_account, client, region, analyzer_arn):
-
-    findings = get_all_findings(client, analyzer_arn, region)
-    logger.info(f"Found {len(findings)} findings for {analyzer_arn} in {region}")
-
-    # dump info about instances to S3 as json
-    for f in findings:
-
-        resource_item = {}
-        resource_item['awsAccountId']                   = target_account.account_id
-        resource_item['awsAccountName']                 = target_account.account_name
-        resource_item['resourceType']                   = FINDING_TYPE
-        resource_item['source']                         = "Antiope"
-        resource_item['configurationItemCaptureTime']   = str(datetime.datetime.now())
-        resource_item['awsRegion']                      = region
-        resource_item['configuration']                  = f
-        resource_item['supplementaryConfiguration']     = {}
-        resource_item['resourceId']                     = f['id']
-        resource_item['resourceCreationTime']           = f['createdAt']
-        resource_item['errors']                         = {}
-        save_resource_to_s3(FINDING_PATH, resource_item['resourceId'], resource_item)
-
-def get_delegated_admin(payer_account):
-
-
-def get_all_findings(client, arn, region):
-    output = []
-    response = client.list_findings(analyzerArn=arn, maxResults=100)
-    while 'nextToken' in response:
-        for f in response['findings']:
-            # Only save IAM Findings in us-east-1
-            if f['resourceType'] == "AWS::IAM::Role" and region != "us-east-1":
-                continue
-            output.append(f)
-        response = client.list_findings(analyzerArn=arn, maxResults=100, nextToken=response['nextToken'])
-    for f in response['findings']:
-        # Only save IAM Findings in us-east-1
-        if f['resourceType'] == "AWS::IAM::Role" and region != "us-east-1":
-            continue
-        output.append(f)
-    return(output)
