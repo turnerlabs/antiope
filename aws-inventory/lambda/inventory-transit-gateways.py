a479c7a5be371d296c76b9d2ccc96da8ff6e9483
David Keppel <david.keppel@warnermediagroup.com>
2020-07-31 14:45:34 -0700

    modified:   inventory-transit-gateways.py

--- aws-inventory/lambda/inventory-transit-gateways.py
+++ aws-inventory/lambda/inventory-transit-gateways.py
@@ -16,7 +16,7 @@ logging.getLogger('botocore').setLevel(logging.WARNING)
 logging.getLogger('boto3').setLevel(logging.WARNING)
 logging.getLogger('urllib3').setLevel(logging.WARNING)
 
-RESOURCE_PATH = "ec2/vpc"
+RESOURCE_PATH = "ec2/transitgateway"
 
 
 def lambda_handler(event, context):
@@ -29,7 +29,7 @@ def lambda_handler(event, context):
             
         for r in target_account.get_regions():
             try:
-                discover_transit_gateway_attachments(target_account, r)
+                discover_transit_gateways(target_account, r)
             except ClientError as e:
                 # Move onto next region if we get access denied. This is probably SCPs
                 if e.response['Error']['Code'] == 'AccessDeniedException':
@@ -53,61 +53,103 @@ def lambda_handler(event, context):
         capture_error(message, context, e, "General Exception for {}: {}".format(message['account_id'], e))
         raise
 
-def discover_transit_gateway_attachments(target_account, region):
-    '''Iterate across all regions to discover transit gateways'''
+def discover_transit_gateways(target_account, region):
+    '''Iterate accross all regions to discover transit gateways'''
     
     ec2_client = target_account.get_client('ec2', region=region)
-    response = ec2_client.describe_transit_gateway_attachments()
-    
-    if response['TransitGatewayAttachments']:
-        resource_item = {}
-        resource_item['awsAccountId']                   = target_account.account_id
-        resource_item['awsAccountName']                 = target_account.account_name
-        resource_item['resourceType']                   = "AWS::EC2::TransitGateway"
-        resource_item['source']                         = "Antiope"
-        resource_item['awsRegion']                      = region
+    response = ec2_client.describe_transit_gateways()
 
-        for tg in response['TransitGatewayAttachments']:
+    if response['TransitGateways']:
+    
+        for tg in response['TransitGateways']:
         
+            resource_item = {}
+            resource_item['awsAccountId']                   = target_account.account_id
+            resource_item['awsAccountName']                 = target_account.account_name
+            resource_item['resourceType']                   = "AWS::EC2::TransitGateway"
+            resource_item['source']                         = "Antiope"
+            resource_item['awsRegion']                      = region
             resource_item['configurationItemCaptureTime']   = str(datetime.datetime.now())
             resource_item['configuration']                  = tg
             resource_item['supplementaryConfiguration']     = {}
-            resource_item['resourceId']                     = tg['ResourceId']
+            resource_item['resourceId']                     = tg['TransitGatewayId']
             resource_item['errors']                         = {}
-
+            
             if 'Tags' in tg:
                 resource_item['tags']                       = parse_tags(tg['Tags'])
-
-            if tg['ResourceType'] == 'vpn':
-                vpn_response = discover_transit_gateway_vpn(ec2_client, tg['TransitGatewayId'], tg['ResourceId'])[0]
-                
-                # We don't need this for the purpose of inventory discovery.
-                del vpn_response['CustomerGatewayConfiguration']
-                
-                resource_item['supplementaryConfiguration']['VpnConnections'] = vpn_response
-                
-            save_resource_to_s3(RESOURCE_PATH, resource_item['resourceId'], resource_item)
+    
+            # Get the transit gateway attachements based on the gateway ID and add it as part of the supplementray configuration
+            attachements = discover_transit_gateway_attachments(ec2_client, tg['TransitGatewayId'])
             
-            logger.info("Discovered Transit Gateways ({}) in account {} for region {}\n".format(resource_item['resourceId'], target_account.account_id, region))
+            for a in attachements:
+                
+                resource_item['supplementaryConfiguration'] = a
+                
+                if a['ResourceType'] == 'tgw-peering':
+                    tg_peering_response = discover_transit_gateway_peering_attachments(ec2_client, a['TransitGatewayId'], a['TransitGatewayAttachmentId'])[0]
+                    resource_item['supplementaryConfiguration']['TransitGatewayPeeringAttachments'] = tg_peering_response
+                
+                if a['ResourceType'] == 'vpc':
+                    vpc_attachments_response = discover_transit_gateway_vpc_attachments(ec2_client, a['TransitGatewayId'], a['ResourceId'])[0]
+                    resource_item['supplementaryConfiguration']['TransitGatewayVpcAttachments'] = vpc_attachments_response
+        
+            # Save files to S3
+            save_resource_to_s3(RESOURCE_PATH, tg['TransitGatewayId'], resource_item)
+       
+            logger.info("Discovered Transit Gateways ({}) in account {} for region {}".format(tg['TransitGatewayId'], target_account.account_id, region))
             logger.debug("Data: {}".format(resource_item))
     else:
         logger.debug("No Transit Gateways found for account {} in region {}".format(target_account.account_id, region))
 
-def discover_transit_gateway_vpn(ec2_client, tg_id, vpn_id):
-    '''Get VPN Configuration based on the Transit Gateway ID'''
-    response = ec2_client.describe_vpn_connections(
+def discover_transit_gateway_attachments(ec2_client, tgId):
+    ''' Get transit gateway attachement based on transit gateway ID'''
+    response = ec2_client.describe_transit_gateway_attachments(
+            Filters=[
+                {
+                'Name': 'transit-gateway-id',
+                'Values': [
+                    tgId,
+                ]
+                },
+            ]
+        )
+      
+    return(response['TransitGatewayAttachments']) 
+        
+def discover_transit_gateway_vpc_attachments(ec2_client, tgId, resourceId):
+    ''' Get transit gateway vpc attachement information based on transit gateway ID and vpc ID'''
+    response = ec2_client.describe_transit_gateway_vpc_attachments(
+        Filters=[
+        {
+            'Name': 'transit-gateway-id',
+            'Values': [
+                tgId,
+            ],
+            'Name': 'vpc-id',
+            'Values': [
+                resourceId,
+            ]
+        },
+        ]    
+    )
+
+    return(response['TransitGatewayVpcAttachments'])
+
+def discover_transit_gateway_peering_attachments(ec2_client, tgId, tgAttachId):
+    '''Get Transit Gateway Peering Attachment configuration based on the Transit Gateway ID'''
+    response = ec2_client.describe_transit_gateway_peering_attachments(
         Filters=[
             {
                 'Name': 'transit-gateway-id',
                 'Values': [
-                    tg_id,
+                    tgId,
                 ],
-                'Name': 'vpn-connection-id',
+                'Name': 'transit-gateway-attachment-id',
                 'Values': [
-                    vpn_id,
+                    tgAttachId,
                 ]
             },
         ]
     )
     
-    return(response['VpnConnections'])
+    return(response['TransitGatewayPeeringAttachments'])
