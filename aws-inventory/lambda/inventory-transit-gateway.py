97007f126f3a839a67b7d12176e46e2675eff4f4
David Hamm <david.hamm@turner.com>
2021-04-15 10:27:50 -0400

    updated calls for services regions

--- aws-inventory/lambda/inventory-transit-gateway.py
+++ aws-inventory/lambda/inventory-transit-gateway.py
@@ -26,8 +26,8 @@ def lambda_handler(event, context):
 
     try:
         target_account = AWSAccount(message['account_id'])
-            
-        for r in target_account.get_regions():
+        # transit gateways not supported in ap-northeast-3  04/14/2021
+        for r in target_account.get_regions(exclude=["ap-northeast-3"]):
             try:
                 discover_transit_gateways(target_account, r)
             except ClientError as e:
@@ -55,14 +55,14 @@ def lambda_handler(event, context):
 
 def discover_transit_gateways(target_account, region):
     '''Iterate accross all regions to discover transit gateways'''
-    
+
     ec2_client = target_account.get_client('ec2', region=region)
     response = ec2_client.describe_transit_gateways()
 
     if response['TransitGateways']:
-    
+
         for tg in response['TransitGateways']:
-        
+
             resource_item = {}
             resource_item['awsAccountId']                   = target_account.account_id
             resource_item['awsAccountName']                 = target_account.account_name
@@ -76,28 +76,28 @@ def discover_transit_gateways(target_account, region):
             resource_item['ARN']                            = tg['TransitGatewayArn']
             resource_item['resourceCreationTime']           = tg['CreationTime']
             resource_item['errors']                         = {}
-            
+
             if 'Tags' in tg:
                 resource_item['tags']                       = parse_tags(tg['Tags'])
-    
+
             # Get the transit gateway attachements based on the gateway ID and add it as part of the supplementray configuration
             attachements = discover_transit_gateway_attachments(ec2_client, tg['TransitGatewayId'])
-            
+
             for a in attachements:
-                
+
                 resource_item['supplementaryConfiguration'] = a
-                
+
                 if a['ResourceType'] == 'tgw-peering':
                     tg_peering_response = discover_transit_gateway_peering_attachments(ec2_client, a['TransitGatewayId'], a['TransitGatewayAttachmentId'])[0]
                     resource_item['supplementaryConfiguration']['TransitGatewayPeeringAttachments'] = tg_peering_response
-                
+
                 if a['ResourceType'] == 'vpc':
                     vpc_attachments_response = discover_transit_gateway_vpc_attachments(ec2_client, a['TransitGatewayId'], a['ResourceId'])[0]
                     resource_item['supplementaryConfiguration']['TransitGatewayVpcAttachments'] = vpc_attachments_response
-        
+
             # Save files to S3
             save_resource_to_s3(RESOURCE_PATH, tg['TransitGatewayId'], resource_item)
-       
+
             logger.info("Discovered Transit Gateways ({}) in account {} for region {}".format(tg['TransitGatewayId'], target_account.account_id, region))
             logger.debug("Data: {}".format(resource_item))
     else:
@@ -115,9 +115,9 @@ def discover_transit_gateway_attachments(ec2_client, tgId):
                 },
             ]
         )
-      
-    return(response['TransitGatewayAttachments']) 
-        
+
+    return(response['TransitGatewayAttachments'])
+
 def discover_transit_gateway_vpc_attachments(ec2_client, tgId, resourceId):
     ''' Get transit gateway vpc attachement information based on transit gateway ID and vpc ID'''
     response = ec2_client.describe_transit_gateway_vpc_attachments(
@@ -132,7 +132,7 @@ def discover_transit_gateway_vpc_attachments(ec2_client, tgId, resourceId):
                 resourceId,
             ]
         },
-        ]    
+        ]
     )
 
     return(response['TransitGatewayVpcAttachments'])
@@ -153,5 +153,5 @@ def discover_transit_gateway_peering_attachments(ec2_client, tgId, tgAttachId):
             },
         ]
     )
-    
+
     return(response['TransitGatewayPeeringAttachments'])
