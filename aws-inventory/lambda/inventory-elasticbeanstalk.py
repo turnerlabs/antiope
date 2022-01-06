b66589f6364641c3f84e6cc5fae5323c0af9f474
Robert Kuhlke <robert.kuhlke@discovery.com>
2022-01-06 09:30:24 -0500

    adding storage gateways and elastic beanstalk to inventory

---
+++ aws-inventory/lambda/inventory-elasticbeanstalk.py
@@ -0,0 +1,122 @@
+# Copyright 2019-2020 Turner Broadcasting Inc. / WarnerMedia
+# Copyright 2021 Chris Farris <chrisf@primeharbor.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import boto3
+from botocore.exceptions import ClientError
+import json
+import os
+import time
+from datetime import datetime, timezone
+from dateutil import tz
+
+from antiope.aws_account import *
+from common import *
+
+import logging
+logger = logging.getLogger()
+logger.setLevel(getattr(logging, os.getenv('LOG_LEVEL', default='INFO')))
+logging.getLogger('botocore').setLevel(logging.WARNING)
+logging.getLogger('boto3').setLevel(logging.WARNING)
+logging.getLogger('urllib3').setLevel(logging.WARNING)
+
+RESOURCE_PATH = "eb/application"
+TYPE = "AWS::ElasticBeanstalk::Application"
+
+
+def lambda_handler(event, context):
+    logger.debug("Received event: " + json.dumps(event, sort_keys=True))
+    message = json.loads(event['Records'][0]['Sns']['Message'])
+    logger.info("Received message: " + json.dumps(message, sort_keys=True))
+
+    try:
+        target_account = AWSAccount(message['account_id'])
+        for r in target_account.get_regions():
+            try:
+                discover_elastic_beanstalk_applications(target_account, r)
+                discover_elastic_beanstalk_environments(target_account, r)
+            except ClientError as e:
+                if e.response['Error']['Code'] == 'AccessDenied':
+                    logger.warning(f"AccessDenied attempting to discover ELBs in {target_account.account_name} ({target_account.account_id}) in region {r}: {e}")
+                    continue
+
+
+    except AntiopeAssumeRoleError as e:
+        logger.error("Unable to assume role into account {}({})".format(target_account.account_name, target_account.account_id))
+        return()
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'UnauthorizedOperation':
+            logger.error("Antiope doesn't have proper permissions to this account")
+            return(event)
+        logger.critical("AWS Error getting info for {}: {}".format(message['account_id'], e))
+        capture_error(message, context, e, "ClientError for {}: {}".format(message['account_id'], e))
+        raise
+    except Exception as e:
+        logger.critical("{}\nMessage: {}\nContext: {}".format(e, message, vars(context)))
+        capture_error(message, context, e, "General Exception for {}: {}".format(message['account_id'], e))
+        raise
+
+
+def discover_elastic_beanstalk_applications(target_account:str, region:str):
+    eb = target_account.get_client('elasticbeanstalk', region=region)
+
+    try:
+        apps = eb.describe_applications() # Returns all items no pagination
+    except ClientError as error:
+        logger.error(f"Elastic Beanstalk Describe Application Error: {error}")
+        return
+    for app in apps:
+        resource_item = {
+            "awsAccountId":                 target_account.account_id,
+            "awsAccountName":               target_account.account_name,
+            "resourceType":                 TYPE,
+            "configurationItemCaptureTime": str(datetime.datetime.now()),
+            "awsRegion":                    region,
+            "configuration":                app,
+            "supplementaryConfiguration":   {},
+            "resourceName":                 app.get("ApplicationName"),
+            "resourceId":                   f"{target_account.acount_id}-{region}-{app.get('ApplicationName')}",
+            "ARN":                          app.get("ApplicationArn"),
+            "resourceCreationTime":         app.get("DateCreated"),
+            "errors":                       {}
+        }
+
+        save_resource_to_s3(RESOURCE_PATH, resource_item.get("resourceId"), resource_item)
+
+
+def discover_elastic_beanstalk_environments(target_account:str, region:str):
+    eb = target_account.get_client('elasticbeanstalk', region=region)
+
+    try:
+        envs = eb.describe_environments() # Returns all items no pagination
+    except ClientError as error:
+        logger.error(f"Elastic Beanstalk Describe Environments Error: {error}")
+        return 
+    for env in envs:
+        resource_item = {
+            "awsAccountId":                 target_account.account_id,
+            "awsAccountName":               target_account.account_name,
+            "resourceType":                 TYPE,
+            "configurationItemCaptureTime": str(datetime.datetime.now()),
+            "awsRegion":                    region,
+            "configuration":                env,
+            "supplementaryConfiguration":   {},
+            "resourceName":                 env.get("EnvironmentName"),
+            "resourceId":                   f"{target_account.acount_id}-{region}-{env.get('EnvironmentName')}",
+            "ARN":                          env.get("EnvironmentArn"),
+            "resourceCreationTime":         env.get("DateCreated"),
+            "errors":                       {}
+        }
+
+        save_resource_to_s3(RESOURCE_PATH, resource_item.get("resourceId"), resource_item)
